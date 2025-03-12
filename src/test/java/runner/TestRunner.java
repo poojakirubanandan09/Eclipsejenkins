6d9603d098ecd5e3f410fa3d8778757a39d9df5a
poojakirubanandan09 <poojakirubanandan09@gmail.com>
2025-03-12 10:43:19 +0530

    Update code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.testng.CucumberOptions;
 	    features = "src/test/resources/features",
 	    glue = "stepDefinitions",
 	   // plugin = {"pretty", "html:target/cucumber-reports"}
-	    plugin = {"pretty" , "json:target/cucumber-reports-json/Cucumber.json"} //Generate JSON report
+	    plugin = {"pretty" , "json:target/cucumber-reports-json/Cucumber.json",
+	    		     "html:target/cucumber-reports-html"
+	    } //Generate JSON report
 	    
 	)
 
